90eb5bccd9fc4cc4a1e749b32239edabcc890b77
stjmm <shetrynajerkme@gmail.com>
2026-04-02 20:11:00 +0200

    parsing, tacky, asm, codegen for BINARY

--- src/x86.c
+++ src/x86.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "x86.h"
 #include "ir.h"
-
-#define REG_NAME_32(r) ((r) == REG_AX ? "eax" : "r10d")
 
 #define EMIT_OPERAND(file, op)      \
     do {                            \
         operand_t _o = (op);        \
         if (_o.type == OPERAND_REG) \
-            fprintf((file), "%%%s", REG_NAME_32(_o.reg)); \
+            fprintf((file), "%%%s", reg_name_32(_o.reg)); \
         else if (_o.type == OPERAND_IMM) \
             fprintf((file), "$%d", _o.imm); \
         else \
@@ -31,6 +30,16 @@ static operator_e convert_binop(ir_binary_op_e op)
     if (op == IR_OP_SUBTRACT) operator = ASM_OP_SUB;
     if (op == IR_OP_MULTIPLY) operator = ASM_OP_MULT;
     return operator;
+}
+
+const char* reg_name_32(asm_reg_e r) {
+    switch (r) {
+        case REG_AX:  return "eax";
+        case REG_DX:  return "edx";
+        case REG_R10: return "r10d";
+        case REG_R11: return "r11d";
+        default:      return "unknown";
+    }
 }
 
 static operand_t convert_val(ir_val_t v)
@@ -55,6 +64,13 @@ static void append_instr(asm_function_t *fn, asm_instr_t *instr)
     fn->last = instr;
 }
 
+static void chain(asm_instr_t *a, asm_instr_t *b) { a->next = b; }
+
+static operand_t reg_operand(asm_reg_e r)
+{
+    return (operand_t){ .type = OPERAND_REG, .reg = r, };
+}
+
 static asm_instr_t *make_mov(operand_t src, operand_t dst)
 {
     asm_instr_t *instr = calloc(1, sizeof(asm_instr_t));
@@ -72,8 +88,7 @@ static void emit_instr(asm_function_t *fn, ir_instr_t *instr)
             asm_instr_t *mov = calloc(1, sizeof(asm_instr_t));
             mov->type = ASM_MOV;
             mov->mov.src = src;
-            mov->mov.dst.type = OPERAND_REG;
-            mov->mov.dst.reg= REG_AX;
+            mov->mov.dst = reg_operand(REG_AX);
             append_instr(fn, mov);
 
             asm_instr_t *ret = calloc(1, sizeof(asm_instr_t));
@@ -111,7 +126,7 @@ static void emit_instr(asm_function_t *fn, ir_instr_t *instr)
                 asm_instr_t *mov1 = calloc(1, sizeof(asm_instr_t));
                 mov1->type = ASM_MOV;
                 mov1->mov.src = src1;
-                mov1->mov.dst.reg = REG_AX;
+                mov1->mov.dst = reg_operand(REG_AX);
                 append_instr(fn, mov1);
 
                 asm_instr_t *cdq = calloc(1, sizeof(asm_instr_t));
@@ -219,65 +234,95 @@ static int asm_phase2(asm_program_t *program)
     return next_offset;
 }
 
-/* Phase 3: Insert allocate_stack, fix memory-memory operations */
-
-static asm_instr_t *splice_replace(asm_function_t *fn, asm_instr_t *prev,
-                                   asm_instr_t *curr, asm_instr_t *chain_first,
-                                   asm_instr_t *chain_last)
+static asm_instr_t *replace_instr(asm_function_t *fn, asm_instr_t *prev,
+                                  asm_instr_t *curr, asm_instr_t *first_new,
+                                  asm_instr_t *last_new)
 {
-    chain_last = curr->next;
-    if (prevc)
+    last_new->next = curr->next;
+
+    if (prev) prev->next = first_new;
+    else fn->first = first_new;
+
+    if (fn->last == curr) fn->last = last_new;
+
+    free(curr);
+    return last_new;
 }
 
+/* Phase 3: Insert allocate_stack, fix memory-memory operations */
 static void asm_phase3(asm_program_t *program, int stack_offset)
 {
-    asm_function_t *function = program->function;
+    asm_function_t *fn = program->function;
 
     // Insert allocate_stack at the beginning of function
-    asm_instr_t *instr = calloc(1, sizeof(asm_instr_t));
-    instr->type = ASM_ALLOCSTACK;
-    instr->allocate_stack.val = stack_offset;
-    instr->next = function->first;
-    function->first = instr;
+    asm_instr_t *alloc = calloc(1, sizeof(asm_instr_t));
+    alloc->type = ASM_ALLOCSTACK;
+    alloc->allocate_stack.val = stack_offset;
+    alloc->next = fn->first;
+    fn->first = alloc;
 
-    asm_instr_t *curr = function->first;
     asm_instr_t *prev = NULL;
+    asm_instr_t *curr = fn->first;
+    
+    operand_t r10 = reg_operand(REG_R10);
+    operand_t r11 = reg_operand(REG_R11);
+
     while (curr) {
         switch (curr->type) {
+            
+            // movl mem1, mem2 -> movl mem1, %r10d / movl %r10d, mem2
             case ASM_MOV: {
-                if (curr->mov.src.type == OPERAND_STACK &&
-                    curr->mov.dst.type == OPERAND_STACK) {
-                    operand_t src = curr->mov.src;
-                    operand_t dst = curr->mov.dst;
-                    operand_t scratch = { .type = OPERAND_REG, .reg = REG_R10 };
+                if (curr->mov.src.type != OPERAND_STACK &&
+                    curr->mov.dst.type != OPERAND_STACK) break;
 
-                    asm_instr_t *mov1 = calloc(1, sizeof(asm_instr_t));
-                    mov1->type = ASM_MOV;
-                    mov1->mov.src = src;
-                    mov1->mov.dst = scratch;
+                asm_instr_t *m1 = make_mov(curr->mov.src, r10);
+                asm_instr_t *m2 = make_mov(r10, curr->mov.dst);
+                chain(m1, m2);
+                curr = replace_instr(fn, prev, curr, m1, m2);
+                break;
+            }
 
-                    asm_instr_t *mov2 = calloc(1, sizeof(asm_instr_t));
-                    mov2->type = ASM_MOV;
-                    mov2->mov.src = scratch;
-                    mov2->mov.dst = dst;
-                    
-                    // Splice into list
-                    if (prev)
-                        prev->next = mov1;
-                    else
-                        function->first = mov1;
+            // addl/subl mem1, mem2 -> movl mem1, %r10d / movl %r10d, mem2
+            case ASM_BINARY: {
+                bool src_mem = curr->binary.src.type == OPERAND_STACK;
+                bool dst_mem = curr->binary.dst.type == OPERAND_STACK;
+                bool is_mul = curr->binary.op == ASM_OP_MULT;
 
-                    mov1->next = mov2;
-                    mov2->next = curr->next;
-
-                    if (function->last == curr)
-                        function->last = mov2;
-
-                    asm_instr_t *old = curr;
-                    curr = mov2;
-                    free(old);
-                    break;
+                if (is_mul && dst_mem) {
+                    // imull src, mem  ->  movl mem, %r11d / imull src, %r11d / movl %r11d, mem
+                    asm_instr_t *m1 = make_mov(curr->binary.dst, r11);
+                    asm_instr_t *op = calloc(1, sizeof(asm_instr_t));
+                    op->type       = ASM_BINARY;
+                    op->binary.op  = ASM_OP_MULT;
+                    op->binary.src = curr->binary.src;
+                    op->binary.dst = r11;
+                    asm_instr_t *m2 = make_mov(r11, curr->binary.dst);
+                    chain(m1, op); chain(op, m2);
+                    curr = replace_instr(fn, prev, curr, m1, m2);
+                } else if (!is_mul && src_mem && dst_mem) {
+                    // addl/subl mem, mem  ->  movl src, %r10d / op %r10d, dst
+                    asm_instr_t *m1 = make_mov(curr->binary.src, r10);
+                    asm_instr_t *op = calloc(1, sizeof(asm_instr_t));
+                    op->type       = ASM_BINARY;
+                    op->binary.op  = curr->binary.op;
+                    op->binary.src = r10;
+                    op->binary.dst = curr->binary.dst;
+                    chain(m1, op);
+                    curr = replace_instr(fn, prev, curr, m1, op);
                 }
+                break;
+            }
+            
+            // idiv $imm -> movl $imm, %r10d / idiv %r10d
+            case ASM_IDIV: {
+                if (curr->idiv.operand.type != OPERAND_IMM) break;
+
+                asm_instr_t *m1 = make_mov(curr->idiv.operand, r10);
+                asm_instr_t *idiv = calloc(1, sizeof(asm_instr_t));
+                idiv->type = ASM_IDIV;
+                idiv->idiv.operand = r10;
+                chain(m1, idiv);
+                replace_instr(fn, prev, curr, idiv, m1);
             }
             default:
                 break;
@@ -307,6 +352,10 @@ void emit_x86(ir_program_t *ir, FILE *file)
                 fprintf(file, "    subq     $%d, %%rsp\n", instr->allocate_stack.val);
                 break;
             }
+            case ASM_CDQ: {
+                fprintf(file, "    cdq\n");
+                break;
+            }
             case ASM_MOV: {
                 operand_t src = instr->mov.src;
                 operand_t dst = instr->mov.dst;
@@ -330,10 +379,32 @@ void emit_x86(ir_program_t *ir, FILE *file)
                 fprintf(file, "\n");
                 break;
             }
+            case ASM_BINARY: {
+                const char *op = NULL;
+                switch(instr->binary.op) {
+                    case ASM_OP_ADD: op = "addl"; break;
+                    case ASM_OP_SUB: op = "subl"; break;
+                    case ASM_OP_MULT: op = "imull"; break;
+                    default: op = "???"; break;
+                }
+                fprintf(file, "    %s     ", op);
+                EMIT_OPERAND(file, instr->binary.src);
+                fprintf(file, ", ");
+                EMIT_OPERAND(file, instr->binary.dst);
+                fprintf(file, "\n");
+                break;
+            }
+            case ASM_IDIV: {
+                fprintf(file, "    idivl    ");
+                EMIT_OPERAND(file, instr->idiv.operand);
+                fprintf(file, "\n");
+                break;
+            }
             case ASM_RET: {
                 fprintf(file, "    movq     %%rbp, %%rsp\n");
                 fprintf(file, "    popq     %%rbp\n");
                 fprintf(file, "    ret\n");
+                break;
             }
         }
         instr = instr->next;
